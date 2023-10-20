fac353926e920ea6165481e7ec15b21667e6c1b9
poshdy <roshdy2810@gmail.com>
2023-10-20 09:40:53 -0700

    commit

--- constants.ts
+++ constants.ts
@@ -1,7 +1,8 @@
 import Netflix from "@/public/Projects/netflix.png";
 import Cryptos from "@/public/Projects/Cryptos.png";
 import Spotify from "@/public/Projects/Spotify.png";
-import Abcity from "@/public/Projects/Abcity.png";
+import Saas from "@/public/Projects/Saas.png";
+
 import SS from "@/public/Projects/SS.png";
 export const NavLinks = [
   { id: "home", title: "Home" },
@@ -13,12 +14,20 @@ export const NavLinks = [
 export const Data = [
   {
     id: "1",
-    title: "Netflix clone",
-    desc: "netflix clone is fullstack application comes with full crud opearations and complete authentication system with SSO using Supabase.",
-    tech: ["Next", "React", "Typescript", "Supabase", "Tailwind"],
-    github: "https://github.com/poshdy/netflixclone",
-    site: "https://netflixclone-rho-lac.vercel.app/",
-    src: Netflix,
+    title: "UR AI PAL",
+    desc: "UR AI PAL is AI Software as a service provides 5 AI services Chat, Code, Image, Video, and Music generations integrating OpenAI and Replicate AI APIS",
+    tech: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "MongoDB",
+      "Prisma",
+      "Stripe",
+      "Tailwind",
+    ],
+    github: "https://github.com/poshdy/ai-saas",
+    site: "https://ai-saas-pearl-ten.vercel.app/",
+    src: Saas,
   },
   {
     id: "2",
@@ -28,9 +37,19 @@ export const Data = [
     github: "https://github.com/poshdy/Tempom",
     site: "https://tempo-poshdy.vercel.app/",
     src: Spotify,
+  }, 
+  {
+
+    id: "3",
+    title: "Netflix clone",
+    desc: "netflix clone is fullstack application comes with full crud opearations and complete authentication system with SSO using Supabase.",
+    tech: ["Next", "React", "Typescript", "Supabase", "Tailwind"],
+    github: "https://github.com/poshdy/netflixclone",
+    site: "https://netflixclone-rho-lac.vercel.app/",
+    src: Netflix,
   },
   {
-    id: "3",
+    id: "4",
     title: "Crytos",
     desc: "All-in-one cryptocurrency tracking app. View the latest prices, get all currency stats, coin data visualization and read the latest crypto news.",
     tech: ["Next", "Typescript", "React", "shdcn ui"],
@@ -38,30 +57,22 @@ export const Data = [
     site: "https://cryptos-seven.vercel.app/",
     src: Cryptos,
   },
-  {
-    id: "4",
-    title: "Sniper Sneaker",
-    desc: "Sniper Sneaker is Sneakers Ecommerce website built with React and Sanity with full shopping cart functionality",
-    tech: [
-      "React",
-      "Javascript",
-      "Redux toolkit",
-      "Sanity.io",
-      "React query",
-      "Tailwind",
-    ],
-    github:
-      "https://github.com/poshdy/SniperSneaker-sanity-ecom/tree/master/snipersneaker",
-    site: "https://snipersneaker.vercel.app/",
-    src: SS,
-  },
   // {
-  //   id: "5",
-  //   title: "ABCITY",
-  //   desc: "Abcity is fitness agency landing page also there is Bmi & Daily calorie calculators",
-  //   tech: ["React", "Javascript", , "Tailwind", "React query"],
-  //   github: "https://github.com/poshdy/ABCITY/tree/master/gbro",
-  //   site: "https://abcity.vercel.app/",
-  //   src: Abcity,
+  //   id: "4",
+  //   title: "Sniper Sneaker",
+  //   desc: "Sniper Sneaker is Sneakers Ecommerce website built with React and Sanity with full shopping cart functionality",
+  //   tech: [
+  //     "React",
+  //     "Javascript",
+  //     "Redux toolkit",
+  //     "Sanity.io",
+  //     "React query",
+  //     "Tailwind",
+  //   ],
+  //   github:
+  //     "https://github.com/poshdy/SniperSneaker-sanity-ecom/tree/master/snipersneaker",
+  //   site: "https://snipersneaker.vercel.app/",
+  //   src: SS,
   // },
+
 ];
